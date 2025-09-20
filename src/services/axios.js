58e95df799415c9f0b2bfebb6e52b76802496ff3
natia <natia.karamnishvili369@cve.tsu.edu.ge>
2025-09-20 13:31:42 +0400

    feat: import axios

---
+++ src/services/axios.js
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+const instance = axios.create({
+    baseURL:"https://api.redseam.redberryinternship.ge/api",
+    headers:{
+        Accept:"application/json",
+        "Content-Type": "application/json"
+    }
+})
+
+export default instance
