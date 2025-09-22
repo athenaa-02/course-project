efa923921baeb8006032f442ccf8c81fcb422657
natia <natia.karamnishvili369@law.tsu.ge>
2025-09-23 00:07:16 +0400

    wip

--- src/views/Registration/Registration.jsx
+++ src/views/Registration/Registration.jsx
@@ -2,31 +2,45 @@ import { useEffect } from "react"
 import { register } from "../../services/auth"
 
 const Registration = () =>{
-useEffect(() =>{
-const getProducts = async () =>{
-    try{
-       const response = await getProductsInfo()
-    }
-    catch(error){
+// useEffect(() =>{
+// const getProducts = async () =>{
+//     try{
+//        const response = await getProductsInfo()
+//     }
+//     catch(error){
 
-    }
- }
-getProducts()
-}, [])
+//     }
+//  }
+// getProducts()
+// }, [])
 
-const submitHandler = async(data) =>{
+    const [formData, setFormData] = useState({
+        email: '',
+        avatar: '',
+        password: '',
+        username: '',
+    })
+
+    const handleChange = (e) =>{
+        setFormData({
+            ...formData,
+        })
+    }
+
+const submitHandler = async(e) =>{
+    e.preventDefault()
 try{
-const response = await register(data)
+const response = await register(formData)
 console.log(response.data)
 }
 catch(error){
-
+console.error('registration failed:', error.response.data)
 }
 }
 
     return <>
 <form onSubmit={submitHandler}>
-    register
+    <input type="email" name="email" value={formData.email} onChange={handleChange} />
     </form>    
 </>
 }
