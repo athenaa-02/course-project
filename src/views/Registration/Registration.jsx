2f4349ee47c9ea6fd68c2711844c6263a420b385
natia <natia.karamnishvili369@law.tsu.ge>
2025-09-23 10:25:49 +0400

    feat: setup registration form structure

--- src/views/Registration/Registration.jsx
+++ src/views/Registration/Registration.jsx
@@ -1,48 +1,75 @@
-import { useEffect } from "react"
-import { register } from "../../services/auth"
+import { useEffect, useState } from "react";
+import { register } from "../../services/auth";
 
-const Registration = () =>{
-// useEffect(() =>{
-// const getProducts = async () =>{
-//     try{
-//        const response = await getProductsInfo()
-//     }
-//     catch(error){
+const Registration = () => {
+  // useEffect(() =>{
+  // const getProducts = async () =>{
+  //     try{
+  //        const response = await getProductsInfo()
+  //     }
+  //     catch(error){
 
-//     }
-//  }
-// getProducts()
-// }, [])
+  //     }
+  //  }
+  // getProducts()
+  // }, [])
 
-    const [formData, setFormData] = useState({
-        email: '',
-        avatar: '',
-        password: '',
-        username: '',
-    })
+  const [formData, setFormData] = useState({
+    email: "",
+    avatar: "",
+    password: "",
+    username: "",
+    confirmPassword:"",
+  });
 
-    const handleChange = (e) =>{
-        setFormData({
-            ...formData,
-        })
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await register(formData);
+      console.log(response.data);
+    } catch (error) {
+      console.error("registration failed:", error.response.data);
     }
+  };
 
-const submitHandler = async(e) =>{
-    e.preventDefault()
-try{
-const response = await register(formData)
-console.log(response.data)
-}
-catch(error){
-console.error('registration failed:', error.response.data)
-}
-}
+  return (
+    <>
+      <input
+        type="text"
+        name="username"
+        value={formData.username}
+        onChange={handleChange}
+      />
+      <form onSubmit={submitHandler}>
+        <input
+          type="email"
+          name="email"
+          value={formData.email}
+          onChange={handleChange}
+        />
+        <input
+          type="password"
+          name="password"
+          value={formData.password}
+          onChange={handleChange}
+        />
+        <input
+          type="password"
+          name="confirmPassword"
+          value={formData.confirmPassword}
+          onChange={handleChange}
+        />
+        
+      </form>
+    </>
+  );
+};
 
-    return <>
-<form onSubmit={submitHandler}>
-    <input type="email" name="email" value={formData.email} onChange={handleChange} />
-    </form>    
-</>
-}
-
-export default Registration
+export default Registration;
